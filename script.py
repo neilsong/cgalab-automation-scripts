c885852abb24464cf7c01c448f8657db62041f40
Neil Song <song.neil.song@gmail.com>
2021-06-22 14:54:18 -0500

    init

---
+++ script.py
@@ -0,0 +1,30 @@
+import fileinput
+from os import listdir
+from os.path import join, basename, split, splitext
+import sys
+
+PATH="/home/cgalab/test"
+files = []
+
+def path_leaf(path):
+    head, tail = split(path)
+    return tail or basename(head)
+
+files += [join(PATH, fname) for fname in listdir(PATH) if join(PATH, fname).endswith('.xml')]
+
+
+for file in files:
+    global xmin
+    # memoize data
+    #rewrite
+    
+    for line in fileinput.input(file,inplace=0):
+        if line.startswith('<xmin>'):
+            xmin=int(line[5:-7])
+        elif line.startswith('<ymin>'):
+            ymin=int(line[5:-7])
+        elif line.startswith('<xmax>'):
+            xmax=int(line[5:-7])
+        elif line.startswith('<ymax>'):
+            ymax=int(line[5:-7])
+        
