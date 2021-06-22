649d6e41f0fe7a93a77da06bdd4661ed632b66b0
Jonathan He <mac@Jonathans-MacBook-Pro.local>
2021-06-22 18:19:21 -0500

    temp save by jonathan, will work tmr

--- jonathanscript.py
+++ jonathanscript.py
@@ -1,9 +1,10 @@
 import fileinput
+import os
 from os import listdir
 from os.path import join, basename, split, splitext
 import sys
 
-PATH="/Users/mac/Desktop/cgalab-transformation"
+PATH="/Users/mac/Desktop/cgalab-transformation/annotations-old"
 files = []
 
 def path_leaf(path):
@@ -12,11 +13,11 @@ def path_leaf(path):
 
 files += [join(PATH, fname) for fname in listdir(PATH) if join(PATH, fname).endswith('.xml')]
 
-
 for file in files:
-    global xmin,ymin,xmax,ymax
+    xmin=0;ymin=0;xmax=0;ymax=0;
+    new_file_name = file[:-(len(os.path.basename(file))+len("annotations-old/"))]+'annotations-new/new_'+os.path.basename(file)
+    nf = open(new_file_name,"w")
     # memoize data
-    #rewrite
     
     for line in fileinput.input(file,inplace=0):
         if(line.startswith('<xmin>')):
@@ -27,16 +28,22 @@ for file in files:
             xmax=int(line[5:-7])
         elif(line.startswith('<ymax>')):
             ymax=int(line[5:-7])
+        #insert alena's code
+
+    #rewrite
     for line in fileinput.input(file,inplace=0):
         if(line.startswith('<xmin>')):
-            xmin=int(line[5:-7])
+            nf.write('<xmin>'+xmin+'</xmin>')
         elif(line.startswith('<ymin>')):
-            ymin=int(line[5:-7])
+            nf.write('<ymin>'+ymin+'</ymin>')
         elif(line.startswith('<xmax>')):
-            xmax=int(line[5:-7])
+            nf.write('<xmax>'+ymin+'</xmax>')
         elif(line.startswith('<ymax>')):
-            ymax=int(line[5:-7])
+            nf.write('<ymax>'+ymin+'</ymax>')
+        else:
+            nf.write(line)
+    nf.close
 
-            
+        
         
         
